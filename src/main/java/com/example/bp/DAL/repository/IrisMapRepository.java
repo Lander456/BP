8cc983808a598b2f17fcd1a4139663261bae61cf
Lander456 <tedus.lp@gmail.com>
2026-02-16 20:39:40 +0100

    [FIX] fixed the mistype error in the class definition

--- src/main/java/com/example/bp/DAL/repository/IrisMapRepository.java
+++ src/main/java/com/example/bp/DAL/repository/IrisMapRepository.java
@@ -1,9 +1,9 @@
 package com.example.bp.DAL.repository;
 
-import com.example.bp.DAL.model.IrisImage;
+import com.example.bp.DAL.model.IrisMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IrisMapRepository extends JpaRepository<IrisImage, Long> {
+public interface IrisMapRepository extends JpaRepository<IrisMap, Long> {
 }
